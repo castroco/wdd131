f61def0936a2fe587f53beb0dbba2aa361656b13
castroco <carlos.castro.p@gmail.com>
2026-08-31 12:18:52 -0400

    test

--- scripts/getdates.js
+++
@@ -1,9 +0,0 @@
-function get_data() {
-    let date = new Date();
-    let full_year = date.getFullYear();
-    const year_span = document.querySelector("#currentyear");
-    let year = `${full_year}`;
-    year_span.innerHTML = year;
-
-    document.querySelector("#lastModified").innerHTML = `Last modified: ${document.lastModified}`;
-}
